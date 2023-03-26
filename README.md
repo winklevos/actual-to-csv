abc3fc80779ceb46a6b40fcc3995367a58bfaf25
winklevos <winklevos@outlook.com>
2023-03-26 18:20:04 +1100

    path handling

--- README.md
+++ README.md
@@ -2,5 +2,16 @@
 
 Extracts CSV from Actual export/backup
 
+This Python script takes an [export](https://actualbudget.github.io/docs/Getting-Started/using-actual/settings#export) from Actual budget and create a flat csv for use in Excel.
 
-This Python script takes an [export](https://actualbudget.github.io/docs/Getting-Started/using-actual/settings#export) from Actual budget and create a flat csv for use in Excel. 
+## Requirements
+
+- Actual Budget Export
+- Python3
+
+## Running
+
+1. Clone the source or script to your local machine
+2. Run the [script](/src/main.py)
+3. Input the full path to the Actual export `.zip`
+4. (Optional) Input a full path for the resulting `.csv`
--- src/main.py
+++ src/main.py
@@ -1,26 +1,36 @@
 import sqlite3
 import csv
 import zipfile
+import os
+
 
 FILENAME = 'db.sqlite'
+TEMP_LOCATION = 'temp/'
 
-SOURCE_NAME = ""
-
-ZIP_PATH = r"" + SOURCE_NAME
-
-archive = zipfile.ZipFile(ZIP_PATH, 'r')
-archive.extract(FILENAME)
-
+HEADERS = [
+    "Transaction Date",
+    "Amount",
+    "Notes",
+    "Imported Description",
+    "Account Name",
+    "Account Type",
+    "Account Off Budget",
+    "Account Closed",
+    "Category",
+    "Category Is Income",
+    "Category Group",
+    "Payee"
+]
 
 TRANSACTION_QUERY = """
-SELECT 
+SELECT
 	substr(t.date,1,4) || '-' || substr(t.date,5,2) || '-' || substr(t.date,7,2) AS "date",
 	t.amount/100.0000,
 	t.notes,
 	t.imported_description,
-	a.name, 
-	a.type, 
-	a.offbudget, 
+	a.name,
+	a.type,
+	a.offbudget,
 	a.closed,
 	c.name AS "category",
 	c.is_income,
@@ -31,36 +41,55 @@ FROM transactions t
 	LEFT JOIN categories c ON t.category = c.id
 	LEFT JOIN category_groups cg ON c.cat_group = cg.id
 	LEFT JOIN payees p ON t.description = p.id
-WHERE 
+WHERE
 t.tombstone = 0
 AND t.starting_balance_flag = 0
 AND t.transferred_id IS NULL
 ORDER BY t.date desc
 """
 
-con = sqlite3.connect(FILENAME)
-cur = con.cursor()
+def main():
 
-res = cur.execute(TRANSACTION_QUERY)
+    zipPath = None
+    csvPath = None
 
-HEADERS = [
-	"Transaction Date",
-	"Amount",
-	"Notes",
-	"Imported Description",
-	"Account Name",
-	"Account Type",
-	"Account Off Budget",
-	"Account Closed",
-	"Category",
-	"Category Is Income",
-	"Category Group",
-	"Payee"
-]
+    while True:
+        try:
+            zipPath = input('Enter path to export zip:')
+
+            if (not os.path.exists(zipPath)):
+                raise Exception(f'File {zipPath} does not exist')
+
+            break
+
+        except Exception as err:
+            print(err)
+
+    csvPath = input('Enter output csv path:')
+
+    if (not csvPath):
+        csvPath = 'ActualTo.csv'
+
+    # extract archive
+    archive = zipfile.ZipFile(zipPath, 'r')
+    archive.extract(FILENAME, TEMP_LOCATION)
+
+    # read sqlite3
+    con = sqlite3.connect(TEMP_LOCATION + FILENAME)
+    cur = con.cursor()
+
+    res = cur.execute(TRANSACTION_QUERY)
+
+    # Write to csv
+    with open(csvPath, 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(HEADERS)
+        writer.writerows(res)
+
+    con.close()
+    # remove temp sqlite.db
+    os.remove(TEMP_LOCATION + FILENAME)
 
 
-with open('', 'w',newline='') as f:
-	writer = csv.writer(f)
-	writer.writerow(HEADERS)
-	writer.writerows(res)
-	
+if __name__ == '__main__':
+    main()
